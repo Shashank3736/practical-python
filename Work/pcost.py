5d7b30767f870244fb894ed2436e49115ee1aced
Shreyash Raj <shreyashr267@gmail.com>
2021-11-02 22:58:22 +0530

    chapter 1 completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
 def portfolio_cost(filename: str):
   cost = 0
   with open(filename, 'rt') as f:
-    next(f)
-    for line in f:
-      row = line.split(',')
-      if len(row) != 3:
-        continue
+    rows = csv.reader(f)
+    next(rows)
+    for row in rows:
       share_price = int(row[1])*(float(row[2]))
       cost = cost + share_price
 
   return cost
 
-cost = portfolio_cost('./Work/Data/portfolio.csv')
-print('Total cost',cost)
+if len(sys.argv) == 2:
+  filename = sys.argv[1]
+else:
+  filename = './Work/Data/portfolio.csv'
+try:
+  cost = portfolio_cost(filename)
+  print('Total cost',cost)
+except FileNotFoundError:
+  print("Error: File not available at",filename)
